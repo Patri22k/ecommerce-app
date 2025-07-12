adc0f40dc3843f9b2da107006d22747c23e7c316
Patri22k <bajzik.patrik6@gmail.com>
2025-07-12 23:12:31 +0200

    feat: - dockerize frontend, backend, payment-service and postgresql backend database - initialize prisma in backend - setup userAuth router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import { authRouter } from './routers/authUser';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors({
-  origin: "*",
+  origin: "*", // Only for development!
   credentials: true,
 }));
+
+app.use('/api', authRouter);
 
 export default app;
---
+++ backend/src/routers/authUser.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/register', (req, res) => {
+  // TODO
+})
+
+router.post('/login', (req, res) => {
+  // TODO
+})
+
+export const authRouter = router;
