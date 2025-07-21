fd9c13545ef5c66048e984dc008449392f4ff7bb
Patri22k <bajzik.patrik6@gmail.com>
2025-07-21 20:52:59 +0200

    feat: - create a seeding script to initialize the 1st admin role defined in .env file - updated models inside schema.prisma
    chore(deps): updated backend dependencies

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env sh
 
+echo "Waiting for Postgres to be ready..."
+
+while ! nc -z postgres 5432; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+echo "Postgres is up - continuing..."
+
 echo "Generating Prisma client..."
 npx prisma generate
 
 echo "Running prisma migrations..."
-npx prisma migrate dev --name init
+npx prisma migrate dev --name init --skip-seed
+
+echo "Running seed script..."
+npx prisma db seed
 
 echo "Starting app..."
 npm start
---
+++ backend/prisma/seed.ts
@@ -0,0 +1,44 @@
+import "dotenv/config";
+import {PrismaClient} from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminEmail = process.env.INITIAL_ADMIN_EMAIL;
+  const adminPassword = process.env.INITIAL_ADMIN_PASSWORD;
+
+  if (!adminEmail || !adminPassword) {
+    throw new Error("INITIAL_ADMIN_EMAIL and INITIAL_ADMIN_PASSWORD must be set in the environment variables.");
+  }
+
+  const existingAdmin = await prisma.user.findFirst({
+    where: { role: "ADMIN" },
+  });
+
+  if (existingAdmin) {
+    throw new Error("An admin with credentials " + adminEmail + " already exists.");
+  }
+
+  const hashedPassword = await bcrypt.hash(adminPassword, 10);
+
+  const admin = await prisma.user.create({
+    data: {
+      name: "Jozko Mrkvicka",
+      email: adminEmail,
+      password: hashedPassword,
+      role: "ADMIN",
+    }
+  });
+
+  console.log(`Admin successfully created: ${admin.email}`);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
